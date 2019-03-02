8a13c18352585013d6ce34cff7139f8f932a5137
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-03-02 15:27:41 -0500

    Add login with email and password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,51 +1,76 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs');
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 // load User model
-const User = require('../../models/User');
+const User = require("../../models/User");
 
 // @route   GET /api/users/test
 // @desc    Test users route
 // @access  Public
-router.get('/test', (req, res) => {
-  res.json({msg: "users works"});
+router.get("/test", (req, res) => {
+  res.json({ msg: "users works" });
 });
 
 // @route   GET /api/users/register
 // @desc    Register user
 // @access  Public
-router.post('/register', (req, res) => {
-  User.findOne({email: req.body.email})
-    .then(user => {
-      if(user) {
-        return res.status(400).json({email: 'Email already exists'});
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", // size
+        r: "pg", // rating
+        d: "mm" // default
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+// @route   GET /api/users/login
+// @desc    Login User / Returning JWT token
+// @access  Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // find user by email
+  User.findOne({ email }).then(user => {
+    // check for user
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    // check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success" });
       } else {
-        const avatar = gravatar.url(req.body.email, {
-          s: '200',   // size
-          r: 'pg',    // rating
-          d: 'mm'     // default
-        });
-
-        const newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          avatar,
-          password: req.body.password
-        });
-
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if(err) throw err;
-            newUser.password = hash;
-            newUser.save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err));
-          });
-        });
+        return res.status(400).json({ password: "Password incorrect" });
       }
     });
+  });
 });
 
 module.exports = router;
