93229a29a330fa7ea7ccab8eb8eae89a45fa1477
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-03-02 01:10:20 -0500

    Connect to the database

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+const dotenv = require('dotenv');
+
+// setting up the dotenv config
+dotenv.config({
+    path: './.env'
+});
+
+const dbPass = process.env.DB_PASS;
+
+
+console.log('------', dbPass);
+module.exports = {
+  mongoURI: `mongodb://admin:${dbPass}@dev-connector-shard-00-00-hcjaz.mongodb.net:27017,dev-connector-shard-00-01-hcjaz.mongodb.net:27017,dev-connector-shard-00-02-hcjaz.mongodb.net:27017/test?ssl=true&replicaSet=dev-connector-shard-0&authSource=admin&retryWrites=true`
+}
