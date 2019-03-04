d855935c254531f3c2e5ae6773e4994cf09a047a
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-03-03 23:33:18 -0500

    Add POST api/posts/like/:id route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -98,4 +98,36 @@ router.delete(
   }
 );
 
+// @route   POST api/posts/like/:id
+// @desc    Like post
+// @access  Private
+router.post(
+  '/like/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    log.info('POST api/posts/like/:id');
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // if user already liked the post
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyliked: 'User already liked this post' });
+          }
+
+          // add user it to likes array
+          post.likes.unshift({ user: req.user.id });
+
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+    });
+  }
+);
+
 module.exports = router;
