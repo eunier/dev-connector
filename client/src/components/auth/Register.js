90f021aeba79cfc81fdf08dd1c924fe04c10a680
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-03-06 22:42:43 -0500

    Enable registration

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Register extends Component {
   constructor() {
@@ -15,11 +16,11 @@ class Register extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
-  onChange = (e) => {
+  onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
-  }
+  };
 
-  onSubmit = (e) => {
+  onSubmit = e => {
     e.preventDefault();
 
     const newUser = {
@@ -29,8 +30,11 @@ class Register extends Component {
       password2: this.state.password2
     };
 
-    console.log(newUser);
-  }
+    axios
+      .post('/api/users/register', newUser)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response.data));
+  };
 
   render() {
     return (
