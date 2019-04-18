487445484c9e9f05d1e75aa6c978b16e315dc57c
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-04-17 22:23:44 -0400

    Add comment form component and action

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import PostItem from '../posts/PostItem';
+import CommentForm from './CommentForm';
 import Spinner from '../common/Spinner';
 import { getPost } from '../../actions/postActions';
 
@@ -21,6 +22,7 @@ class Post extends Component {
       postContent = (
         <div>
           <PostItem post={post} showActions={false} />
+          <CommentForm postId={post._id} />
         </div>
       );
     }
@@ -45,7 +47,7 @@ class Post extends Component {
 Post.propTypes = {
   getPost: PropTypes.func.isRequired,
   post: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = state => ({
   post: state.post
