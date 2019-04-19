62c346aed481c594cda4195ac162d84579521dde
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-04-19 11:47:54 -0400

    Setup environment variables

--- config/keys.js
+++ config/keys.js
@@ -1,13 +1,5 @@
-const dotenv = require('dotenv');
-
-// setting up the dotenv config
-dotenv.config({
-  path: './.env'
-});
-
-const dbPass = process.env.DB_PASS;
-
-module.exports = {
-  mongoURI: `mongodb://admin:${dbPass}@dev-connector-shard-00-00-hcjaz.mongodb.net:27017,dev-connector-shard-00-01-hcjaz.mongodb.net:27017,dev-connector-shard-00-02-hcjaz.mongodb.net:27017/test?ssl=true&replicaSet=dev-connector-shard-0&authSource=admin&retryWrites=true`,
-  secretOrKey: 'secret'
-};
+if (process.env.MODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
