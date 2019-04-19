d1f3c21521d75ded0d06b0abb0ab83dfaaa179ae
Yunier Alvarez <yunieralvarezdev@icloud.com>
2019-04-19 16:15:07 -0400

    Fix mobile deployment issue

--- client/src/store.js
+++ client/src/store.js
@@ -5,13 +5,15 @@ import rootReducer from './reducers';
 const initialState = {};
 
 const middleware = [thunk];
+const reduxDevtoolExtension =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    reduxDevtoolExtension
   )
 );
 
