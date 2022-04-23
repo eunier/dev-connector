57cd1a21ac40e4f7d5d4796dfc073c9fc58330c1
Yunier Alvarez <yunieralvarez@gmail.com>
2022-04-23 19:14:35 -0400

    rename

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParsere = require('body-parser');
+const bodyParser = require('body-parser');
 const passport = require('passport');
 const log = require('./icon-log/icon-log');
 const path = require('path');
@@ -15,8 +15,8 @@ const posts = require('./routes/api/posts');
 const app = express();
 
 // body parse middleware
-app.use(bodyParsere.urlencoded({ extended: false }));
-app.use(bodyParsere.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // db config
 const db = require('./config/keys').mongoURI;
